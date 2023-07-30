023e1e213abf1b13983bd0ee5deb508ee0698ec6
KimDanwoo <dudgh9198@naver.com>
2023-07-30 16:35:28 +0900

    ✏️ stack 문제풀이

---
+++ stack/기능개발.js
@@ -0,0 +1,29 @@
+function solution(progresses, speeds) {
+  const answer = []
+  const daysLeft = progresses.map((progress, index) =>
+    Math.ceil((100 - progress) / speeds[index])
+  )
+
+  let count = 1 // 배포되는 기능 개수를 카운트하기 위한 변수
+  let maxDay = daysLeft[0] // 최대 남은 일수를 저장하는 변수
+
+  for (let i = 1; i < daysLeft.length; i++) {
+    if (daysLeft[i] <= maxDay) {
+      count++
+    } else {
+      count = 1
+      maxDay = daysLeft[i]
+    }
+  }
+  answer.push(count)
+  return answer
+}
+
+console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
+/**
+ * 풀이 시간 :
+ *
+ * 풀이 과정 :
+ *
+ * 풀이 참고 및 Learning :
+ */
