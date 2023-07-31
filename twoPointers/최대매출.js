7bca90aff42b8a81c3f26fd3478f8f10e4123226
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-07-31 13:53:40 +0900

    ✏ 최대매출 문제풀이중

---
+++ twoPointers/최대매출.js
@@ -0,0 +1,19 @@
+function solution(n, arr) {
+  let result = 0,
+    hap = 0
+  for (let i = 0; i < n; i++) hap += arr[i]
+  for (let i = n; i < arr.length; i++) {
+    hap += arr[i] = arr[i - n]
+    result = Math.max(result, hap)
+  }
+  return result
+}
+
+console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]))
+/**
+ * 풀이 시간 :
+ *
+ * 풀이 과정 :
+ *
+ * 풀이 참고 및 Learning :
+ */
