a1322cd3b3c3419e30bd9d121f0f140107d96fae
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-07-31 14:37:07 +0900

    ✏ 슬라이딩윈도우 알고리즘 풀이

--- twoPointers/최대매출.js
+++ twoPointers/최대매출.js
@@ -3,7 +3,7 @@ function solution(n, arr) {
     hap = 0
   for (let i = 0; i < n; i++) hap += arr[i]
   for (let i = n; i < arr.length; i++) {
-    hap += arr[i] = arr[i - n]
+    hap += arr[i] - arr[i - n]
     result = Math.max(result, hap)
   }
   return result
@@ -13,7 +13,8 @@ console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15]))
 /**
  * 풀이 시간 :
  *
- * 풀이 과정 :
- *
+ * 풀이 과정 : 슬라이딩 윈도우 알고리즘을 사용해
+ * for문이 한번 돌 동안 3개씩 더한 최고값을 리턴해줬다.
+ * 슬라이딩 윈도우 알고리즘 개념 이해용
  * 풀이 참고 및 Learning :
  */
