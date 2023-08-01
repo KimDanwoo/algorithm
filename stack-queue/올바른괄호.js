f1b54f624ce329a225125d076062b7caf6f95c8d
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-08-01 21:09:35 +0900

    ✏ stack문제 풀이

--- stack-queue/올바른괄호.js
+++ stack-queue/올바른괄호.js
@@ -1,48 +1,21 @@
-function solution(s) {
-  return s.split("").reduce(
-    (acc, curr, i) => {
-      if (curr === "(") {
-        acc.개수[0] += 1;
+function solution(brackets) {
+  let stack = []
+  const isOpenBracket = (b) => b === '('
+  const isMachBracket = (open, close) => open === '(' && close === ')'
+  for (let i = 0; i < brackets.length; i++) {
+    if (isOpenBracket(brackets[i])) {
+      stack.push(brackets[i])
+    } else {
+      const last = stack.pop()
+      if (!last || !isMachBracket(last, brackets[i])) {
+        return false
       }
-      if (curr === ")") {
-        acc.개수[1] += 1;
-      }
-      const [여는괄호갯수, 닫는괄호갯수] = acc.개수;
-      if (여는괄호갯수 < 닫는괄호갯수) {
-        acc.result = false;
-      }
-      if (i === s.split("").length - 1 && curr === "(") {
-        acc.result = false;
-      }
-
-      return acc;
-    },
-    { 개수: [0, 0], result: true }
-  ).result;
-}
-
-function solution(s) {
-  let count = 0;
-
-  for (paren of s) {
-    if (paren === "(") {
-      count++;
-    }
-    if (paren === ")") {
-      count--;
-    }
-
-    if (count < 0) {
-      return false;
     }
   }
-
-  if (count !== 0) {
-    return false;
-  }
-
-  return true;
+  return stack.length === 0
 }
+
+console.log(solution('(()(()))(())'))
 
 /**
  * 풀이 시간 : 12:18 ~ 12:45
@@ -52,6 +25,10 @@ function solution(s) {
  * ) 가 더 많아지면 안 된다.
  * 마지막이 )로 끝나야 한다.
  * 총 갯수가 맞아야 한다.
+ *
+ * for of로 제출시 효율성 테스트가 통과하지 못하는데 왜그런지 조금 찾아봐야겠다.
+ * 효율성이 무슨 30점 밖에 안나오지??
+ * for 문을 사용하고 마지막 리턴값을 length를 비교하는게 true를 리턴하는것 보다 효율성이 좋은것같다.
  *
  * 풀이 참고 및 Learning :
  * for of 생각보다 가독성도 좋다.
